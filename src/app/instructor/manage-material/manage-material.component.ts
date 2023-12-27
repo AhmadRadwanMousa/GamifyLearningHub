42c75df0b729522889675f23376f8a3ec00c4265
AHMAD MOUSA <armousa19@cit.just.edu.jo>
2023-12-27 18:48:18 +0300

    fixcoursesequence

--- src/app/instructor/manage-material/manage-material.component.ts
+++ src/app/instructor/manage-material/manage-material.component.ts
@@ -2,24 +2,23 @@ import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { InstructorService } from 'src/app/Services/instructor.service';
-import { ViewChild} from '@angular/core';
-import {MatAccordion, MatExpansionModule} from '@angular/material/expansion';
+import { ViewChild } from '@angular/core';
+import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
 
 import { ViewChildren, QueryList } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
-
 @Component({
   selector: 'app-manage-material',
   templateUrl: './manage-material.component.html',
-  styleUrls: ['./manage-material.component.scss']
+  styleUrls: ['./manage-material.component.scss'],
 })
-
-
-
 export class ManageMaterialComponent {
-
-  constructor(public _instructorService: InstructorService, public dialog: MatDialog,private router: Router) {}
+  constructor(
+    public _instructorService: InstructorService,
+    public dialog: MatDialog,
+    private router: Router
+  ) {}
   ngOnInit() {
     this._instructorService.GetSectionsByUserId();
   }
@@ -28,7 +27,6 @@ export class ManageMaterialComponent {
   filesName: any;
   filesPath: any;
 
-  
   @ViewChildren(MatAccordion) accordions: QueryList<MatAccordion> | undefined;
   @ViewChild('CreateDialog') CreateCourseSectionDialog: any;
   @ViewChild('UpdateDialog') UpdateCourseSectionDialog: any;
@@ -37,9 +35,6 @@ export class ManageMaterialComponent {
   @ViewChild('ConfirmationLectureDialog') ConfirmationDeleteLectureDialog: any;
   @ViewChild('UpdateLectureDialog') UpdateLectureDialog: any;
 
-
-  
-
   CreateCourseSectionForm: FormGroup = new FormGroup({
     coursesectionname: new FormControl('', Validators.required),
     coursesectionduration: new FormControl('', Validators.required),
@@ -47,21 +42,19 @@ export class ManageMaterialComponent {
   });
 
   UpdateCourseSectionForm: FormGroup = new FormGroup({
-    coursesectionid : new FormControl(),
+    coursesectionid: new FormControl(),
     coursesectionname: new FormControl('', Validators.required),
     coursesectionduration: new FormControl('', Validators.required),
     sectionid: new FormControl(),
     section: new FormControl(),
-    lectures : new FormControl(),
+    lectures: new FormControl(),
   });
-
-
 
   CreateLectureForm: FormGroup = new FormGroup({
     coursesectionid: new FormControl(),
     lecturename: new FormControl('', Validators.required),
     lecturefile: new FormControl(),
-    lectureduration :  new FormControl('', Validators.required),
+    lectureduration: new FormControl('', Validators.required),
   });
 
   UpdateLectureForm: FormGroup = new FormGroup({
@@ -69,116 +62,117 @@ export class ManageMaterialComponent {
     coursesectionid: new FormControl(),
     lecturename: new FormControl('', Validators.required),
     lecturefile: new FormControl(),
-    lectureduration :  new FormControl('', Validators.required),
+    lectureduration: new FormControl('', Validators.required),
     coursesection: new FormControl(),
-    userprogresses :  new FormControl(),
+    userprogresses: new FormControl(),
   });
-
-
 
   openAllPanels() {
     console.log('Opening all panels');
     if (this.accordions) {
-      this.accordions.forEach(accordion => accordion.openAll());
+      this.accordions.forEach((accordion) => accordion.openAll());
     }
   }
 
-  
   closeAllPanels() {
     console.log('Closing all panels');
     if (this.accordions) {
-      this.accordions.forEach(accordion => accordion.closeAll());
+      this.accordions.forEach((accordion) => accordion.closeAll());
     }
   }
-  
 
-
-  loadMaterial(sectionId : number){
+  loadMaterial(sectionId: number) {
     this._instructorService.GetAllMaterialsBySectionId(sectionId);
     console.log(this.selectedSection);
   }
-
 
   OpenCreateDialog() {
     this.dialog.open(this.CreateCourseSectionDialog);
   }
 
   CreateCourseSection() {
-    this.CreateCourseSectionForm.controls['sectionid'].setValue(this.selectedSection);
+    this.CreateCourseSectionForm.controls['sectionid'].setValue(
+      this.selectedSection
+    );
 
-    this._instructorService.CreateCourseSection(this.CreateCourseSectionForm.value);
+    this._instructorService.CreateCourseSection(
+      this.CreateCourseSectionForm.value
+    );
   }
 
   CreateLecture() {
     this.CreateLectureForm.controls['lecturefile'].setValue(this.filesPath);
 
-    this._instructorService.CreateLecture(this.CreateLectureForm.value , this.selectedSection);
+    this._instructorService.CreateLecture(
+      this.CreateLectureForm.value,
+      this.selectedSection
+    );
   }
 
-  OpenCreateLectureDialog(id : number) {
+  OpenCreateLectureDialog(id: number) {
     this.CreateLectureForm.controls['coursesectionid'].setValue(id);
     // this.CreateLectureForm.controls['lecturefile'].setValue(this.filesPath);
 
     this.dialog.open(this.CreateLectureDialog);
   }
 
-  OpenUpdateDialog(data : any){
+  OpenUpdateDialog(data: any) {
     this.UpdateCourseSectionForm.setValue(data);
     this.dialog.open(this.UpdateCourseSectionDialog);
+  }
+
+  OpenUpdateLectureDialog(data: any) {
+    this.UpdateLectureForm.setValue(data);
+    this.dialog.open(this.UpdateLectureDialog);
+  }
+
+  UpdateCourseSection() {
+    this._instructorService.UpdateCourseSection(
+      this.UpdateCourseSectionForm.value
+    );
+  }
+
+  UpdateLecture() {
+    this.UpdateLectureForm.controls['lecturefile'].setValue(this.filesPath);
+
+    this._instructorService.UpdateLecture(
+      this.UpdateLectureForm.value,
+      this.selectedSection
+    );
+  }
+
+  OpenConfirmDialog(id: number) {
+    var dialog = this.dialog.open(this.ConfirmationDeleteDialog);
+    dialog.afterClosed().subscribe((result) => {
+      if (result == 'yes') this.DeletCourseSection(id);
+    });
+  }
+
+  OpenConfirmLectureDialog(lectureId: number) {
+    var dialog = this.dialog.open(this.ConfirmationDeleteLectureDialog);
+    dialog.afterClosed().subscribe((result) => {
+      if (result == 'yes') this.DeletLecture(lectureId);
+    });
+  }
+
+  DeletLecture(lectureId: number) {
+    this._instructorService.DeleteLecture(lectureId, this.selectedSection);
+  }
+
+  DeletCourseSection(id: number) {
+    this._instructorService.DeleteCourseSection(id, this.selectedSection);
+  }
+
+  UploadFiles(event: any) {
+    let fileToUpload = event.target.files[0] as File;
+    if (!fileToUpload) {
+      return;
     }
-
-    OpenUpdateLectureDialog(data : any){
-      this.UpdateLectureForm.setValue(data);
-      this.dialog.open(this.UpdateLectureDialog);
-    }
-
-    UpdateCourseSection(){
-
-      this._instructorService.UpdateCourseSection(this.UpdateCourseSectionForm.value);
-    }
-
-    UpdateLecture(){
-      this.UpdateLectureForm.controls['lecturefile'].setValue(this.filesPath);
-
-      this._instructorService.UpdateLecture(this.UpdateLectureForm.value ,  this.selectedSection);
-    }
-
-    
-    OpenConfirmDialog(id: number) {
-
-      var dialog = this.dialog.open(this.ConfirmationDeleteDialog);
-      dialog.afterClosed().subscribe((result) => {
-        if (result == 'yes') this.DeletCourseSection(id);
-      });
-    }
-
-    OpenConfirmLectureDialog(lectureId: number) {
-      var dialog = this.dialog.open(this.ConfirmationDeleteLectureDialog);
-      dialog.afterClosed().subscribe((result) => {
-        if (result == 'yes') this.DeletLecture(lectureId);
-      });
-    }
-
-
-    DeletLecture(lectureId: number) {
-
-      this._instructorService.DeleteLecture(lectureId , this.selectedSection);
-    }
-
-    DeletCourseSection(id: number) {
-      this._instructorService.DeleteCourseSection(id , this.selectedSection);
-    }
-
-    UploadFiles(event: any) {
-      let fileToUpload = event.target.files[0] as File;
-      if (!fileToUpload) {
-        return;
-      }
-      this.filesName = fileToUpload.name;
-      const formData = new FormData();
-      formData.append('file', fileToUpload);
-      this._instructorService.UploadFiles(formData).subscribe((path: string) => {
-        this.filesPath = path;
-      });
-    }
+    this.filesName = fileToUpload.name;
+    const formData = new FormData();
+    formData.append('file', fileToUpload);
+    this._instructorService.UploadFiles(formData).subscribe((path: string) => {
+      this.filesPath = path;
+    });
+  }
 }
