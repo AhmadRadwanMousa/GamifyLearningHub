dec4aeb5b125444100f7905585c83b430ba85aad
Muhammad Al-Shurman <108474043+muhammadshurman@users.noreply.github.com>
2024-01-12 00:02:19 +0300

    Angular finished

---
+++ src/app/report-filter.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ReportFilterPipe } from './report-filter.pipe';
+
+describe('ReportFilterPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ReportFilterPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
